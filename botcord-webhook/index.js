d927addfc2b6c8acd0c3e51eaf2d9bd9acbf2cc6
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-09 14:53:28 -0800

    access token

--- botcord-webhook/index.js
+++ botcord-webhook/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -40,7 +42,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
     
   // Parse the query params
   let mode = req.query['hub.mode'];
