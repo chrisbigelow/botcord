aa42ef6e81adf5a971782349d2e28449d7c134d6
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-10 13:22:36 -0800

    changed json

--- botcord-webhook/index.js
+++ botcord-webhook/index.js
@@ -98,7 +98,6 @@ function handleMessage(sender_psid, received_message) {
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     response = {
-      "message":{
         "text": "Thanks for the image!",
         "quick_replies":[
           {
